42608e30b967c2c96f2dd6065f27388f8aecc0c2
Leandro Andrade Vieira <65571034+Leandro1105@users.noreply.github.com>
2024-10-26 12:40:54 -0300

    Delete backend directory - Estava com erro

--- backend/src/main/java/com/example/hackathon/application/questions/persistence/QuestionsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hackathon.application.questions.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionsRepository extends JpaRepository<QuestionsEntity, Long> {
-
-}
